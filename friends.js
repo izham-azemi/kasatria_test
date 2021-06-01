49f51bc4c3aa12f577edc868c3ca74ac805b30e2
Mohamad Izham Azemi <izham.azemi@mimos.my>
2021-06-02 00:01:22 +0800

    first commit

---
+++ friends.js
@@ -0,0 +1,705 @@
+export default
+{
+	"friends": [
+		{
+		  "name": "Modo",
+		  "age" : "32",
+		  "gender": "M"	  
+		},
+		{
+		  "name": "Saifi",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Kamalsha",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "TheShred",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Iron",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Awal",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Lieya",
+		  "age" : "32",
+		  "gender": "F"
+		},
+		{
+		  "name": "Barjoyai",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Sharez",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Aliff",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nasri",
+		  "age" : "55",
+		  "gender": "M"
+		},
+		{
+		  "name": "Muhamad",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Maizatul",
+		  "age" : "39",
+		  "gender": "F"
+		},
+		{
+		  "name": "Rahmat",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Baharul",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hamsiah",
+		  "age" : "60",
+		  "gender": "F"
+		},
+		{
+		  "name": "Khu",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Sze Chan",
+		  "age" : "40",
+		  "gender": "F"
+		},
+		{
+		  "name": "Affizin",
+		  "age" : "37",
+		  "gender": "M"
+		},
+		{
+		  "name": "Inchick",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Alpandi",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Izham",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Afiq",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hazim",
+		  "age" : "12",
+		  "gender": "M"
+		},
+		{
+		  "name": "Zakaria",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hairey",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Azriq",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Amar",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Azfan",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Arrazi",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Irfan",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Ikhbar",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Mariyani",
+		  "age" : "56",
+		  "gender": "F"
+		},
+		{
+		  "name": "Piya",
+		  "age" : "32",
+		  "gender": "F"
+		},
+		{
+		  "name": "Marwan",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Mus",
+		  "age" : "50",
+		  "gender": "M"
+		},
+		{
+		  "name": "Kasyim",
+		  "age" : "50",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nurshakirin",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Azizan",
+		  "age" : "50",
+		  "gender": "F"
+		},
+		{
+		  "name": "Fatin",
+		  "age" : "33",
+		  "gender": "F"
+		},
+		{
+		  "name": "Razirah",
+		  "age" : "36",
+		  "gender": "F"
+		},
+		{
+		  "name": "Ema",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Zahili",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nadia",
+		  "age" : "32",
+		  "gender": "F"
+		},
+		{
+		  "name": "Suria",
+		  "age" : "40",
+		  "gender": "F"
+		},
+		{
+		  "name": "Jumasri",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Syaiful",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Syamimi",
+		  "age" : "25",
+		  "gender": "F"
+		},
+		{
+		  "name": "Shariman",
+		  "age" : "42",
+		  "gender": "M"
+		},
+		{
+		  "name": "Firdaus",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Fadhlullah",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Irwan",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Tie-ha",
+		  "age" : "33",
+		  "gender": "F"
+		},
+		{
+		  "name": "Ayim",
+		  "age" : "27",
+		  "gender": "M"
+		},
+		{
+		  "name": "Chad",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Fariha",
+		  "age" : "29",
+		  "gender": "F"
+		},
+		{
+		  "name": "Fakhri",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Maimunah",
+		  "age" : "56",
+		  "gender": "F"
+		},
+		{
+		  "name": "Syafika",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Fikri",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nazri",
+		  "age" : "50",
+		  "gender": "M"
+		},
+		{
+		  "name": "Aiman",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hafiz",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Ayu",
+		  "age" : "38",
+		  "gender": "F"
+		},
+		{
+		  "name": "Faril",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Izzati",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Kamarul",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Faizz",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Abbashah",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hanif",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Sharifah",
+		  "age" : "35",
+		  "gender": "F"
+		},
+		{
+		  "name": "Azira",
+		  "age" : "35",
+		  "gender": "F"
+		},
+		{
+		  "name": "Syasha",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Syazmira",
+		  "age" : "23",
+		  "gender": "F"
+		},
+		{
+		  "name": "Nana",
+		  "age" : "29",
+		  "gender": "F"
+		},
+		{
+		  "name": "Azni",
+		  "age" : "40",
+		  "gender": "F"
+		},
+		{
+		  "name": "Suhana",
+		  "age" : "36",
+		  "gender": "F"
+		},
+		{
+		  "name": "Azizi",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Syazwana",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Syazwanie",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Adibah",
+		  "age" : "32",
+		  "gender": "F"
+		},
+		{
+		  "name": "Adi",
+		  "age" : "49",
+		  "gender": "M"
+		},
+		{
+		  "name": "Ajaque",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Neo",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hidayah",
+		  "age" : "33",
+		  "gender": "F"
+		},
+		{
+		  "name": "Fasya",
+		  "age" : "33",
+		  "gender": "F"
+		},
+		{
+		  "name": "Meow",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Zulfadli",
+		  "age" : "38",
+		  "gender": "M"
+		},
+		{
+		  "name": "Asyraq",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Anwar",
+		  "age" : "39",
+		  "gender": "M"
+		},
+		{
+		  "name": "Azila",
+		  "age" : "42",
+		  "gender": "F"
+		},
+		{
+		  "name": "Amir",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Izran",
+		  "age" : "33",
+		  "gender": "M"
+		},
+		{
+		  "name": "Jasdy",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Zara",
+		  "age" : "36",
+		  "gender": "F"
+		},
+		{
+		  "name": "Johari",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Syahwan",
+		  "age" : "30",
+		  "gender": "M"
+		},
+		{
+		  "name": "Asilah",
+		  "age" : "30",
+		  "gender": "F"
+		},
+		{
+		  "name": "Mustafa",
+		  "age" : "70",
+		  "gender": "M"
+		},
+		{
+		  "name": "Intan",
+		  "age" : "40",
+		  "gender": "F"
+		},
+		{
+		  "name": "Nabila",
+		  "age" : "27",
+		  "gender": "F"
+		},
+		{
+		  "name": "Baizura",
+		  "age" : "35",
+		  "gender": "F"
+		},
+		{
+		  "name": "Firdaus",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nazmi",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Manisah",
+		  "age" : "57",
+		  "gender": "F"
+		},
+		{
+		  "name": "Suliana",
+		  "age" : "36",
+		  "gender": "F"
+		},
+		{
+		  "name": "Khairool",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Fauzan",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Asma",
+		  "age" : "35",
+		  "gender": "F"
+		},
+		{
+		  "name": "Shamsi",
+		  "age" : "38",
+		  "gender": "M"
+		},
+		{
+		  "name": "Momok",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Zulkifli",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Syafiq",
+		  "age" : "30",
+		  "gender": "M"
+		},
+		{
+		  "name": "Murad",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Amir",
+		  "age" : "30",
+		  "gender": "M"
+		},
+		{
+		  "name": "Ain",
+		  "age" : "35",
+		  "gender": "M"
+		},
+		{
+		  "name": "Badlishah",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Munirah",
+		  "age" : "36",
+		  "gender": "F"
+		},
+		{
+		  "name": "Syahidah",
+		  "age" : "26",
+		  "gender": "F"
+		},
+		{
+		  "name": "Halim",
+		  "age" : "39",
+		  "gender": "M"
+		},
+		{
+		  "name": "Nuruddin",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hay",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Idzwan",
+		  "age" : "30",
+		  "gender": "M"
+		},
+		{
+		  "name": "Fadzl",
+		  "age" : "48",
+		  "gender": "M"
+		},
+		{
+		  "name": "Shahirah",
+		  "age" : "28",
+		  "gender": "F"
+		},
+		{
+		  "name": "Hazna",
+		  "age" : "47",
+		  "gender": "F"
+		},
+		{
+		  "name": "Aina",
+		  "age" : "38",
+		  "gender": "F"
+		},
+		{
+		  "name": "Tamilarasi",
+		  "age" : "50",
+		  "gender": "F"
+		},
+		{
+		  "name": "Fauzi",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Faridah",
+		  "age" : "55",
+		  "gender": "F"
+		},
+		{
+		  "name": "Zeta",
+		  "age" : "37",
+		  "gender": "F"
+		},
+		{
+		  "name": "Shazwani",
+		  "age" : "31",
+		  "gender": "F"
+		},
+		{
+		  "name": "Hizam",
+		  "age" : "40",
+		  "gender": "M"
+		},
+		{
+		  "name": "Munzir",
+		  "age" : "36",
+		  "gender": "M"
+		},
+		{
+		  "name": "Faizal",
+		  "age" : "31",
+		  "gender": "M"
+		},
+		{
+		  "name": "Hidayah",
+		  "age" : "29",
+		  "gender": "F"
+		},
+		{
+		  "name": "Mahadie",
+		  "age" : "45",
+		  "gender": "M"
+		},
+		{
+		  "name": "Aizzat",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Alimin",
+		  "age" : "32",
+		  "gender": "M"
+		},
+		{
+		  "name": "Azlin",
+		  "age" : "13",
+		  "gender": "F"
+		}
+	 ]
+}
